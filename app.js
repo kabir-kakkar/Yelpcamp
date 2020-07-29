bda6c2eca1582ed620f0c09cdc298deb00392594
kabir <kabir.kakkar4@gmail.com>
2020-07-29 19:20:19 +0530

    version 2 with data persistence completed

--- app.js
+++ app.js
@@ -1,42 +1,87 @@
-const { request } = require("express");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose")
 
-var express = require("express");
-const bodyParser = require("body-parser");
-var app = express();
-
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Granite Hill", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Salmon Creek", image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Granite Hill", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"}
-];
+//Schema Setup
+var campgroundSchema = new mongoose.Schema ({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Adding a Campground Manually
+// Campground.create(
+//     {
+//         name: "Granite Hill", 
+//     image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350",
+//     description: "This is a huge Granite Hill Section!"
+// }, function (err, campground) {
+//     if (err) {
+//         console.log (err);
+//     } else {
+//         console.log ("CAMPGROUND ADDED");
+//         console.log (campground);
+//     }
+// });
 
 app.get("/", function(req, res){
     res.render ("landing.ejs");
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
-    
-    res.render("campgrounds.ejs", {campgrounds: campgrounds});
+    // Get all campgrounda from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if (err) {
+            console.log (err);
+        } else {
+            res.render("index.ejs", {campgrounds: allCampgrounds});
+        }
+    });  
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs");
 });
 
+//CREATE - add new campground to DB
 app.post("/campgrounds", function (req, res){
     // get data from form
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
-    // add to campground array
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description: description}; 
+    
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(3000, function(){
