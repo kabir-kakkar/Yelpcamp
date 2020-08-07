dd703a816f390c5e843442ae66751d78d3d7ce4c
kabir <kabir.kakkar4@gmail.com>
2020-08-07 21:55:23 +0530

    Bug Fixed Listening to route

--- app.js
+++ app.js
@@ -56,6 +56,5 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
-    console.log("The YelpCamp Server Has Started!");
- });
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, process.env.IP);
